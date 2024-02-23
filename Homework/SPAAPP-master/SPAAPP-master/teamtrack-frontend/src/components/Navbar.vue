4db42e45dbee4361db1fb078865a68a96c5e6404
henri <henrievert32@gmail.com>
2024-02-23 09:47:26 +0200

    navbar upgrade

--- Homework/SPAAPP-master/SPAAPP-master/teamtrack-frontend/src/components/Navbar.vue
+++ Homework/SPAAPP-master/SPAAPP-master/teamtrack-frontend/src/components/Navbar.vue
@@ -22,6 +22,23 @@ export default {
   will-change: filter;
   transition: filter 300ms;
 }
+a{
+  color: black;
+  font-family: Comic Sans;
+  padding-left: 2mm;
+  padding-right: 5mm;
+}
+
+a:hover{
+  color: black;
+  animation: color-change 14ms infinite;
+  font-weight: bold;
+}
+
+@keyframes color-change {
+  0% { color: whitesmoke; }
+  100% { color: black; }
+}
 
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
